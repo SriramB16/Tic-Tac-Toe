1557e5e43dfcc961e3f48804cf6810114d590f76
Sriram <136231040+SriramB16@users.noreply.github.com>
2024-10-16 22:21:51 +0530

    functions have been updated

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -6,6 +6,7 @@ export default function Square({value, onSquareClick}){
     // function handleClick(){
     //     setValue('X')
     // }
+    
 
     return(
         <button className='square' onClick={onSquareClick}>{value}</button>
