daece09fbdce74b5ce319e948298e824feed959b
Sriram <136231040+SriramB16@users.noreply.github.com>
2024-10-23 06:33:08 +0530

    deploy tic-tac-toe

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/Learn-React/docs/Tic-Tac-Toe/',
+  plugins: [react()]
 })
