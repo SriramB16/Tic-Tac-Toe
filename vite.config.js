84e8cde42f05917dbf7677a6686537d00ae60364
Sriram <136231040+SriramB16@users.noreply.github.com>
2024-10-23 06:50:16 +0530

    Fix homepage in package.json and deployment paths

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Learn-React/docs/Tic-Tac-Toe/',
+  base: '/Learn-React/',
   plugins: [react()]
 })
